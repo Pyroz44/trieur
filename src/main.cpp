43ca4b14237b39417e36ba1af3a28424c9bab4fb
Pyroz44 <adamhelal48@gmail.com>
2026-02-02 10:51:59 +0100

    corrections

--- src/main.cpp
+++ src/main.cpp
@@ -9,61 +9,55 @@
 #define PIN_ON      26
 #define PIN_PWM     27
 
-// Paramètres PWM
+// Paramètres PWM corrigés
 const int pwmFreq = 25000;
 const int pwmChannel = 0;
-const int pwmResolution = 12;
-// Calcul résolution : 80MHz / 25kHz = 3200 pas max
+// 11 bits (Max 2047)
+const int pwmResolution = 11; 
 
 rgb_lcd lcd;
 
 void setup() {
   Serial.begin(115200);
 
-  // Init LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
-  lcd.setRGB(255, 255, 255);
-  lcd.print("Test Moteur");
+  lcd.setRGB(0, 255, 0);
+  lcd.print("Moteur OK ?");
 
-  // Configuration IHM
   pinMode(PIN_BTN1, INPUT_PULLUP);
   pinMode(PIN_BTN2, INPUT_PULLUP);
-
-  // Configuration Moteur
   pinMode(PIN_DIR, OUTPUT);
   pinMode(PIN_ON, OUTPUT);
 
-  // Configuration LEDC (PWM)
   ledcSetup(pwmChannel, pwmFreq, pwmResolution);
   ledcAttachPin(PIN_PWM, pwmChannel);
 
-  // Conditions initiales
-  digitalWrite(PIN_ON, HIGH); // Driver actif
+  digitalWrite(PIN_ON, HIGH); 
   digitalWrite(PIN_DIR, LOW);
-  ledcWrite(pwmChannel, 0);   // Vitesse nulle
+  ledcWrite(pwmChannel, 0);   
 }
 
 void loop() {
-  // Lecture consigne vitesse
   int rawPot = analogRead(PIN_POT);
-  // Mise à l'échelle (0-4095 -> 0-3200)
-  int dutyCycle = map(rawPot, 0, 4095, 0, 3200);
+  
+  // On mappe vers 2047 (la valeur max de 11 bits)
+  int dutyCycle = map(rawPot, 0, 4095, 0, 2047);
 
-  // Gestion sens de rotation
   if (digitalRead(PIN_BTN1) == LOW) {
     digitalWrite(PIN_DIR, LOW);
-    lcd.setCursor(0, 1);
-    lcd.print("Sens: Horaire   ");
+    lcd.setCursor(0, 1); lcd.print("Sens: H   ");
   } 
   else if (digitalRead(PIN_BTN2) == LOW) {
     digitalWrite(PIN_DIR, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print("Sens: Anti-Hor  ");
+    lcd.setCursor(0, 1); lcd.print("Sens: AH  ");
+  } else {
+     lcd.setCursor(0, 1); lcd.print("Attente...");
   }
 
-  // Application de la commande
   ledcWrite(pwmChannel, dutyCycle);
+  
+  Serial.print("PWM: "); Serial.println(dutyCycle);
   
   delay(50);
 }
